ea5e39f8c9814dff4ca9081adca58a639f71d13f
Prashant Rawat <prashantrawat2mx@gmail.com>
2023-02-07 01:13:09 +0530

    Minor fixes

--- src/components/Chat/Message.jsx
+++ src/components/Chat/Message.jsx
@@ -6,14 +6,17 @@ import ShareableLink from '../UI/ShareableLink';
 
 const Message = forwardRef(({ own, msg }, ref) => {
   const msgRef = useRef();
+  console.log(msg);
   
   useImperativeHandle(ref, () => msgRef, [])
 
   return (
        <div ref={msgRef} className={cls["message"] + ` ${own ? cls["own"] : ""}`}>
-          <div className={cls["text"]}>{msg.text.startsWith('__^__') ? 
-              <ShareableLink own={own} content={msg.text.replace('__^__', '')} 
-                  to={msg.text.replace('__^__', '')} /> : msg.text}
+          <div className={cls["text"]}>{msg.text.includes('__^__') ? 
+              <ShareableLink own={own} to={msg.text.replace('__^__', '')} >
+                {msg.text.replace('__^__', '')}
+              </ShareableLink> :
+                 msg.text}
           </div>  
            <div className={cls["time"]}>{format(msg.time)}</div> 
        </div> 
--- src/components/UI/ShareableLink.jsx
+++ src/components/UI/ShareableLink.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import cls from "./ShareableLink.module.css";
 import { Link } from 'react-router-dom';
 
-const ShareableLink = ({ className, to, content, own, children }) => {
+const ShareableLink = ({ className, to, own, children }) => {
   const classes = className + ' linkStyles ' + cls['shareable-link'] + ' ' + (own && cls['own']);
 
   return (
